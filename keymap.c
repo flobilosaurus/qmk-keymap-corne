f7d269fb2ecdb66d8c4d3972bf9f8cf2371567b0
Florian Balling <florian.balling@maibornwolff.de>
2025-04-11 12:03:37 +0200

    feat: add left command key to lower layer

--- keymap.c
+++ keymap.c
@@ -75,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       XXXXXXX,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX, TMUX_W_L, TMUX_P_H, XXXXXXX, TMUX_SEL, XXXXXXX,                   KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, KC_RSFT, XXXXXXX,
+      XXXXXXX, TMUX_W_L, TMUX_P_H, KC_LEFT_GUI, TMUX_SEL, XXXXXXX,               KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, KC_RSFT, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       XXXXXXX, XXXXXXX,TMUX_P_X,TMUX_W_C,TMUX_P_V, XXXXXXX,                      XXXXXXX, XXXXXXX, KC_COMM,  KC_DOT, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
